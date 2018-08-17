c0e71ace6dfd4abb846013894a65d4e200970a17
sverigo <kw74sv@gmail.com>
2018-08-18 01:09:39 +0300

    fixed appspec

--- deploy/configure_software.sh
+++ deploy/configure_software.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+sudo chmod -R 777 /var/www/rails-app
+cd /var/www/rails-app
+
 export DATABASE_ROOT_PASSWORD=$(aws ssm get-parameter --name RAILS-DATABASE-ROOT-PASSWORD --with-decryption --query 'Parameter.Value' --output text --region us-west-2)
 export DATABASE_HOST=$(aws cloudformation describe-stacks --query 'Stacks[?contains(StackId,`rails-stack`)]|[0].Outputs[?contains(OutputKey,`RDSAddress`)]|[].OutputValue' --output text --region us-west-2)
 
 sudo sed -i s/SED_REPLACE_DATABASE_ROOT_PASSWORD/$DATABASE_ROOT_PASSWORD/g /var/www/rails-app/config/database.yml
 sudo sed -i s/SED_REPLACE_DATABASE_HOST/$DATABASE_HOST/g /var/www/rails-app/config/database.yml
 
-cd /var/www/rails-app
 bundle install
-sudo chmod -R 777 /var/www/rails-app
 
 # bundle exec rake db:migrate
 
